9bdc85082c18c8360cd2279a9d8149facd9bdbec
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-04-05 11:55:43 +0100

    Adding if_else_for.R

---
+++ if_else_for.R
@@ -0,0 +1,12 @@
+
+# a template for an if, else statement with a for loop embedded in R
+
+if ('<condition>') {
+  for ('<iterable>') {
+    #
+    # do some operation here
+    #
+  }
+} else {
+  # do this
+}
